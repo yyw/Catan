768da5bd8ddba5d3071390eae2b01f9b562e8e91
Yin Yin Wu <yinyinwu5@yahoo.com>
2010-09-05 15:54:50 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CatanApp::Application.config.secret_token = '63c4783beb36be63afdd3c80e488381a07c64be42ae353e1e9ef0795fc641a26abe9c54effa0514b200c52bc266b8d737e2256e4542db19055f01caceb83cade'
